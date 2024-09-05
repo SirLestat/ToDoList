144fb178ce358dc8365812bc92854820921baaab
Alejandro Santiago <lesthath_@hotmail.com>
2024-09-05 13:44:19 -0600

    nvbnvnvbvbvnvbnvb

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,10 @@
-import { Button, Container, containerClasses, Typography } from "@mui/material";
-import Box from "@mui/material/Box";
-import { lightBlue, red } from "@mui/material/colors";
-import ArrowUpwardOutlinedIcon from "@mui/icons-material/ArrowUpwardOutlined";
-import Grid2 from "@mui/material";
+import React, { useState } from "react";
+import { ThemeProvider, createTheme, Button } from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid2";
+import darkBackground from "./assets/bg-desktop-dark.jpg";
+import lightBackground from "./assets/bg-desktop-light.jpg";
 
-function App() {
-  return (
-    <Container
-      sx={{
-        border: 5,
-        boxShadow: 20,
-        backgroundColor: "lightgreen",
-      }}
-    >
-      <h1>App</h1>
-      <Box
-        sx={{
-          border: 3,
-          p: 5,
-          borderColor: "peru",
-          bgcolor: "#111",
-          color: "white",
-          mb: 5,
-          boxShadow: 10,
-        }}
-      >
-        Pero que hermoso es MUI
-      </Box>
 
-      <Button variant="contained"> Un botón</Button>
-      <Button variant="contained" endIcon={<ArrowUpwardOutlinedIcon />}>
-        Enviar
-      </Button>
-
-      <Grid2 container gap={1}>
-        <Grid2 item xs={12} sm={6}>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
-            temporibus labore quo, nisi, illum earum cupiditate est, eaque unde
-            ex aliquam quas voluptas explicabo porro similique corrupti
-            molestias sit facilis!
-          </p>
-        </Grid2>
-
-        <Grid2 item xs={12} s>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos
-            reiciendis accusamus adipisci nisi harum impedit. Quas similique
-            recusandae quod enim! Fugit possimus dolorum, distinctio velit
-            exercitationem tempora minus dolores accusantium?
-          </p>
-        </Grid2>
-      </Grid2>
-    </Container>
-  );
-}
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,25 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
 import { CssBaseline, createTheme, ThemeProvider } from "@mui/material";
+import AppCopy from "./AppCopy.jsx";
 
-
-
-const theme = createTheme ({
+const theme = createTheme({
   palette: {
-    type: 'light',
+    type: "light",
     primary: {
-      main: '#004d40',
+      main: "#004d40",
     },
     secondary: {
-      main: '#bf360c',
+      main: "#bf360c",
     },
-  }
-})
+  },
+});
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
       <CssBaseline />
+      <AppCopy />
     </ThemeProvider>
-    
   </StrictMode>
 );
